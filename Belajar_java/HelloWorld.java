391dc0ff6d70f152f5adadfadff1f6e16cd96176
Aditya Anantama Sulaikan <adit.sulaikan@gmail.com>
2019-12-05 12:41:31 +0700

    edit helloworld

--- Belajar_java/HelloWorld.java
+++ Belajar_java/HelloWorld.java
@@ -1,5 +1,21 @@
-public class helloWorld{
-	public statis void main(String[]args){
+public class HelloWorld{
+	public static void main(String[] args){
 		System.out.println("Hello World !");
+		car bmw = new car(); //bangun object mobil bmw
+		bmw.merk = "BMW M850i";
+		bmw.maju();
+		
+		car ford = new car();
+		ford.merk = "Ford Mustang GT500";
+		ford.maju();
+	}
+} 
+
+class mobil{
+	//attribut
+	public String merk;
+	//method
+	public void maju(){
+		System.out.println("Mobil "+this.merk+" maju !!! Brmm...");
 	}
 }
